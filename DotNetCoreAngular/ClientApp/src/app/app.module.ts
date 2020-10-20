d44f6f90bf524dd2d6ad39eb2b2773a7dc526a5c
Ravindra Singh <ravindrasingh446@gmail.com>
2020-10-20 17:58:17 +0530

    remove space from app.module.ts file

--- DotNetCoreAngular/ClientApp/src/app/app.module.ts
+++ DotNetCoreAngular/ClientApp/src/app/app.module.ts
@@ -4,7 +4,6 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
